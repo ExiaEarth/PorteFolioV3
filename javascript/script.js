7fcf7bc9a007d23e6a7c6e112a654bc94a0780b5
Lemer marc <jsfs-02@acer-02.interface3namur.local>
2022-07-20 09:28:40 +0200

    V1.0.6((remise en état)

--- javascript/script.js
+++ javascript/script.js
@@ -3,22 +3,16 @@ const tl = gsap.timeline();
 tl.from(".letter",{duration:1,opacity:0,stagger:0.25,});
 tl.to(".letter",{duration:0.5,y:"20%",opacity:0,stagger});
 
-// tl.to(".material-icons",{opacity:1,})
+let hr=document.getElementById();
+hr.from("#sidBarHover",{opacity:1})
 
-// tl.to(".material-icons",{duration:.6,y:5,ease:"circ.in",repeat:-1,yoyo:true})
+// hover.addEventListener("mouseover", function( event ) {
+//     event.target.style.color = "red";
+// }
+// )false;
 
 
-// function reveal() {
-//     var reveals = document.querySelectorAll(".reveal");
-//     for (var i = 0; i < reveals.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = reveals[i].getBoundingClientRect().top;
-//       var elementVisible = 150;
-//       if (elementTop < windowHeight - elementVisible) {
-//         reveals[i].classList.add("active");
-//       } 
-//       else {
-//         reveals[i].classList.remove("active");
-//       }
-//     }
-//   }
+// (function(){
+//     let anTitle=gsap.
+
+// })();
