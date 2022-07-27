7bfbdb9e569ea784f4e1d0f008710b31ef33b7ed
Lemer marc <jsfs-02@acer-02.interface3namur.local>
2022-07-27 19:25:17 +0200

    V1.0.7(Navbar fini+re mise en place de contact)

--- javascript/script.js
+++ javascript/script.js
@@ -3,16 +3,5 @@ const tl = gsap.timeline();
 tl.from(".letter",{duration:1,opacity:0,stagger:0.25,});
 tl.to(".letter",{duration:0.5,y:"20%",opacity:0,stagger});
 
-let hr=document.getElementById();
-hr.from("#sidBarHover",{opacity:1})
-
-// hover.addEventListener("mouseover", function( event ) {
-//     event.target.style.color = "red";
-// }
-// )false;
-
-
-// (function(){
-//     let anTitle=gsap.
-
-// })();
+const tlSid=gsap.timeline();
+tlSid.from("")
