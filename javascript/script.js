13281901306bcf01e2212a89758c151718f53c7c
Marclemer <marc-lemer@hotmail.com>
2022-08-29 17:40:43 +0200

    finition de CONTACT et replacement de JS projetn°1

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,13 @@ tl.to(".letter",{duration:0.5,y:"20%",opacity:0,stagger});
 
 const tlSid=gsap.timeline();
 tlSid.from("")
+
+
+function validEnvoie(){
+   let userFirstName=document.getElementById("userFirstName").value;
+   let userLastName=document.getElementById("userName").value;
+   const retourValid=document.getElementById("retourUser");
+   
+   retourValid.innerHTML="Bonjour"+userFirstName+" "+userLastName+"Votre demande a bien été envoyé";
+//    retourValid.classList.add("")
+}
